bbf75332fe51c43a484f592f8a398381a10ff77e
Patrick Welsh <patrickswelsh@gmail.com>
2024-01-27 13:03:37 -0500

    gitignore updates and starting to edit function

--- functions/stripe-webhook/index.ts
+++ functions/stripe-webhook/index.ts
@@ -2,18 +2,17 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-console.log("Hello from Functions!")
+import Stripe from "npm:stripe";
+
+const stripe = Stripe(Deno.env.get("STRIPE_API_KEY"));
+
+const server = Deno.listen({ port: 8080 });
+
+
+console.log("Hello from Stipe Webhook!")
 
 Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
 })
 
 /* To invoke locally:
